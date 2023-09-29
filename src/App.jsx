02b5b7f5f264c76a1d673c5104ac26ffc47caaad
rahin505 <trahinhasan@gmail.com>
2023-09-29 19:45:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1>Price Options</h1>
+    </>
+  );
+}
+
+export default App;
